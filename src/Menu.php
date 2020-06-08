b359f4931db04cc7ea8501c721fcb6670a2d10cf
Paul Water <p.r.water@setbased.nl>
2020-06-08 06:44:56 +0200

    Remove method itemId().

--- src/Menu.php
+++ src/Menu.php
@@ -10,17 +10,6 @@ interface Menu
 {
   //--------------------------------------------------------------------------------------------------------------------
   /**
-   * Returns the ID of the HTML element of the menu item associated with a page.
-   *
-   * @param int $mnuId The ID of the menu.
-   * @param int $pagId The ID of the page.
-   *
-   * @return string|null
-   */
-  public function itemId(int $mnuId, int $pagId): ?string;
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
    * Returns the HTML code of a menu.
    *
    * @param int $mnuId The ID of the menu.
