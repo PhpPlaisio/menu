406e4bdf834728ee01428a7af710afcc941fba47
Paul Water <p.r.water@setbased.nl>
2020-06-11 10:43:42 +0200

    Add name parameter.

--- src/MenuGenerator.php
+++ src/MenuGenerator.php
@@ -12,11 +12,12 @@ interface MenuGenerator
   /**
    * Returns the HTML code of a menu.
    *
-   * @param int $mnuId The ID of the menu.
+   * @param int         $mnuId The ID of the menu.
+   * @param string|null $name  The name of the menu. Can be used for generating HTML ids and classes.
    *
    * @return string
    */
-  public function menu(int $mnuId): string;
+  public function menu(int $mnuId, ?string $name): string;
 
   //--------------------------------------------------------------------------------------------------------------------
 }
